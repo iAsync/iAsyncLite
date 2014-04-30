f40f51d95d1d750c6cfa3ff255c0a19c4b435ba5
dodikk <dodikk88.reg@gmail.com>
2014-04-30 13:14:31 +0300

    [appledoc] Added reference

--- lib/JFFAsyncOperations/CachedAsyncOperations/JFFPropertyExtractor.h
+++ lib/JFFAsyncOperations/CachedAsyncOperations/JFFPropertyExtractor.h
@@ -4,20 +4,50 @@
 
 @class JFFPropertyPath;
 
+
+/**
+ A class to hold the information about a property that is being asynchronously loaded.
+ */
 @interface JFFPropertyExtractor : NSObject
 
-// Do not pass properties with primitive types (int/BOOL/...)
+/**
+ Property path. The notation is the same as the one used for KVC.
+
+ WARNING : Do not pass properties with primitive types (int/BOOL/...)
+ */
 @property ( nonatomic ) JFFPropertyPath* propertyPath;
+
+/**
+ ???
+ */
 @property ( nonatomic ) NSObject* object;
+
+
 
 //object related data
 @property (nonatomic) NSMutableArray *delegates;
+
+/**
+ A block to cancel object loading.
+ */
 @property (nonatomic, copy) JFFCancelAsyncOperation cancelBlock;
+
+/**
+ A block to start loading the object.
+ */
 @property (nonatomic, copy) JFFAsyncOperation asyncLoader;
+
+/**
+ A block to notify the user about operation success or failure.
+ */
 @property (nonatomic, copy) JFFDidFinishAsyncOperationHandler didFinishBlock;
 
+/**
+ */
 @property (nonatomic) id property;
 
+/**
+ */
 - (void)clearData;
 
 @end
