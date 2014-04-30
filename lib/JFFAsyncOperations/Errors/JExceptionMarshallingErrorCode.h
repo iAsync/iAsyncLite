8c975fe965a3731526b23d55d99eedeff4cb777a
dodikk <dodikk88.reg@gmail.com>
2014-04-30 18:59:16 +0300

    [doc] Added reference for exception marshalling error codes

--- lib/JFFAsyncOperations/Errors/JExceptionMarshallingErrorCode.h
+++ lib/JFFAsyncOperations/Errors/JExceptionMarshallingErrorCode.h
@@ -5,11 +5,34 @@
 #import <Foundation/Foundation.h>
 
 
+/**
+ Error codes that are used for exceptions marshalling.
+ They will be used to create appropriate NSError objects.
+ */
 typedef NS_ENUM( NSInteger, JExceptionMarshallingErrorCode )
 {
+    /**
+     No error has occured.
+     */
     JNoExceptionErrorCode         = 0,
+    
+    /**
+     A subclass of NSException has been thrown. 
+     See JObjcExceptionWrapperError class reference.
+     */
     JObjcExceptionErrorCode          ,
+    
+    /**
+     A subclass of std::exception has been thrown.
+     See JCppExceptionWrapperError class reference.
+     */
     JStdCppExceptionErrorCode        ,
+
+    
+    /**
+     An unknown exception has been thrown and suppressed by the ```catch(...)``` statement.
+     See JUnknownCppExceptionWrapperError class reference.
+     */
     JUnknownCppExceptionErrorCode
 };
 
