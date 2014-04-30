3dc411d0f6ac340b61a5222757f821ce4f1cbc53
dodikk <dodikk88.reg@gmail.com>
2014-04-30 19:05:36 +0300

    [doc] Added reference for execution order enum

--- lib/JFFAsyncOperations/LoadBalancer/JFFQueueExecutionOrder.h
+++ lib/JFFAsyncOperations/LoadBalancer/JFFQueueExecutionOrder.h
@@ -3,10 +3,27 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ This enum describes the strategy of asynchronous operations processing by the JFFLimitedLoadersQueue class.
+ */
 typedef NS_ENUM(NSInteger, JFFQueueExecutionOrder)
 {
+    /**
+     "First In - First Out" - FIFO.
+     The default queue order.
+     */
     JQOrderFifo   = 0,
+    
+    /**
+     "Last In - First Out" - LIFO.
+     The recently added operations have the highest priority.
+     */
     JQOrderStack  = 1,
+    
+    /**
+     The order does not matter. The tasks are executed in a random order.
+     A uniform distribution is used.
+     */
     JQOrderRandom = 2,
 };
 
