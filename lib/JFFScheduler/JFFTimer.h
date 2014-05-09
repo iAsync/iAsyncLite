0038220cd4cec3cdfe43ac602c534dcd8b3be346
dodikk <dodikk88.reg@gmail.com>
2014-05-09 18:54:27 +0300

    [doc] Added reference for JFFScheduler library

--- lib/JFFScheduler/JFFTimer.h
+++ lib/JFFScheduler/JFFTimer.h
@@ -3,31 +3,88 @@
 typedef void (^JFFCancelScheduledBlock) (void);
 typedef void (^JFFScheduledBlock) (JFFCancelScheduledBlock cancel);
 
+
+/**
+This class allows scheduling blocks on the background queue.
+*/
 @interface JFFTimer : NSObject
 
-//returns the shared timer
+
+/**
+A preferred singleton initializer. 
+
+@return A properly initialized shared instance.
+*/
 + (instancetype)sharedByThreadTimer;
 
-//Add new block to timer which will be invoked on the current thread using the default mode after a delay,
-//returning the block for canceling this invocation
-//the invocation will be canceled at removing "timer" object from memory
+
+/**
+Add new block to timer which will be invoked on the current thread using the default mode after a delay.
+The invocation will be canceled at removing "timer" object from memory.
+
+@param block A block to execute later.
+@param duration Time interval for delaying the block.
+
+@return The block to cancel a scheduled operation.
+*/
 - (JFFCancelScheduledBlock)addBlock:(JFFScheduledBlock)block
                            duration:(NSTimeInterval)duration;
 
+
+/**
+Add new block to timer which will be invoked on the current thread using the default mode after a delay.
+The invocation will be canceled at removing "timer" object from memory.
+
+@param block A block to execute later.
+@param duration Time interval for delaying the block.
+@param leeway  Any fire of the timer may be delayed by the system in order to improve power consumption and system
+     performance. The upper limit to the allowable delay may be configured with the leeway argument, the
+     lower limit is under the control of the system.
+
+@return The block to cancel a scheduled operation.
+*/
 - (JFFCancelScheduledBlock)addBlock:(JFFScheduledBlock)block
                            duration:(NSTimeInterval)duration
                              leeway:(NSTimeInterval)leeway;
 
+
+
+/**
+Add new block to timer which will be invoked on the current thread using the default mode after a delay.
+The invocation will be canceled at removing "timer" object from memory.
+
+@param block A block to execute later.
+@param duration Time interval for delaying the block.
+@param dispatchQueue The dispatch queue to which the event handler block is submitted.
+
+@return The block to cancel a scheduled operation.
+*/
 - (JFFCancelScheduledBlock)addBlock:(JFFScheduledBlock)block
                            duration:(NSTimeInterval)duration
                       dispatchQueue:(dispatch_queue_t)dispatchQueue;
 
+
+/**
+Add new block to timer which will be invoked on the current thread using the default mode after a delay.
+The invocation will be canceled at removing "timer" object from memory.
+
+@param block A block to execute later.
+@param duration Time interval for delaying the block.
+@param leeway  Any fire of the timer may be delayed by the system in order to improve power consumption and system
+     performance. The upper limit to the allowable delay may be configured with the leeway argument, the
+     lower limit is under the control of the system.
+@param dispatchQueue The dispatch queue to which the event handler block is submitted.
+
+@return The block to cancel a scheduled operation.
+*/
 - (JFFCancelScheduledBlock)addBlock:(JFFScheduledBlock)block
                            duration:(NSTimeInterval)duration
                              leeway:(NSTimeInterval)leeway
                       dispatchQueue:(dispatch_queue_t)dispatchQueue;
 
-//cancel all delayed invocations for self
+/**
+Cancel all delayed invocations for self.
+*/
 - (void)cancelAllScheduledOperations;
 
 @end
