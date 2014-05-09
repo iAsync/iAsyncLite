4d057f289c8a17a1d6db4b3d160453000f1cc191
dodikk <dodikk88.reg@gmail.com>
2014-05-09 12:41:34 +0300

    [doc] Added reference for NSArray+Empty category

--- lib/JFFUtils/NSArray/NSArray+IsEmpty.h
+++ lib/JFFUtils/NSArray/NSArray+IsEmpty.h
@@ -1,7 +1,17 @@
 #import <Foundation/Foundation.h>
 
+
+/**
+A category to check whether the array is empty or not.
+This approach is more human friendly comparing to [NSArray lastObject].
+*/
 @interface NSArray (IsEmpty)
 
+/**
+Checks whether the array is empty.
+
+@return YES - the array contains at least one object. NO - otherwise.
+*/
 - (BOOL)hasElements;
 
 @end
