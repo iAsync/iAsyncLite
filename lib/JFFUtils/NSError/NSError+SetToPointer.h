6c0bc99818ac7afe61eb49585fd6bcafd5f9cf64
dodikk <dodikk88.reg@gmail.com>
2014-05-09 12:49:57 +0300

    [doc] Added reference for the error assignment category

--- lib/JFFUtils/NSError/NSError+SetToPointer.h
+++ lib/JFFUtils/NSError/NSError+SetToPointer.h
@@ -1,7 +1,19 @@
 #import <Foundation/Foundation.h>
 
+
+/**
+This category automates safe error object assignment to the out parameter pointer.
+You can write it in a single statement and get rid of "if" statements.
+*/
 @interface NSError (SetToPointer)
 
-- (BOOL)setToPointer:(NSError *__autoreleasing *)outError;
+/**
+Sets the "self" error object to the out pointer.
+
+@param outError An out parameter to assign the error to.
+
+@return YES - in case of successful assignment. NO - in case "outError" is NULL.
+*/
+- (BOOL)setToPointer:(out NSError *__autoreleasing *)outError;
 
 @end
