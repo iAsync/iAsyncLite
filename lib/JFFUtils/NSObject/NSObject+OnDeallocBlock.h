606681b86d6e081d8819e322da115cbf95befb04
dodikk <dodikk88.reg@gmail.com>
2014-05-09 13:02:25 +0300

    [doc] Added reference for the "OnDeallocBlock" hook

--- lib/JFFUtils/NSObject/NSObject+OnDeallocBlock.h
+++ lib/JFFUtils/NSObject/NSObject+OnDeallocBlock.h
@@ -1,8 +1,22 @@
 #import <Foundation/Foundation.h>
 
+
+/**
+A category for adding and removing the hook for the [NSObject dealloc] method.
+*/
 @interface NSObject (OnDeallocBlock)
 
+/**
+Adds a hook to the dealloc method.
+The block is invoked on the same thread as the [NSObject dealloc] method.
+*/
 - (void)addOnDeallocBlock:(void(^)(void))block;
+
+
+/**
+Removes a hook from the dealloc method.
+It must be the very same object that you've passed to the [NSObject addOnDeallocBlock:]
+*/
 - (void)removeOnDeallocBlock:(void(^)(void))block;
 
 @end
--- lib/JFFUtils/NSObject/NSObject+Ownerships.h
+++ lib/JFFUtils/NSObject/NSObject+Ownerships.h
@@ -1,14 +1,38 @@
 #import <JFFUtils/Blocks/JUContainersHelperBlocks.h>
-
 #import <Foundation/Foundation.h>
 
+
+/**
+A category for memory management of the "NSObject+OnDeallocBlock" category.
+*/
 @interface NSObject (Ownerships)
 
-//lazy load property, any object can be added to this array
+
+/**
+Adds an object to be retained by **self**.
+All ownersips are released in the [NSObject dealloc] method.
+
+@param object An object that will be retained by **self**.
+*/
 - (void)addOwnedObject:(id)object;
 
+
+/**
+Removes an object retained by **self**. 
+The [NSArray removeObject:] is used under the hood.
+
+@param object An object that was previously added to **self**.
+*/
 - (void)removeOwnedObject:(id)object;
 
+
+/**
+Searches through the attached objects.
+
+@param predicate A predicate to search through attached objects.
+
+@return The object retained by **self** matching the given predicate.
+*/
 - (id)firstOwnedObjectMatch:(JFFPredicateBlock)predicate;
 
 @end
