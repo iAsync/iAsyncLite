cf059b2c1b3a5e41dcea98d19d897f42f0766f9e
dodikk <dodikk88.reg@gmail.com>
2014-05-10 17:15:05 +0300

    [doc] Added reference for auto cancel operations

--- lib/JFFAsyncOperations/NSObject+AutoCancelAsyncOperation.h
+++ lib/JFFAsyncOperations/NSObject+AutoCancelAsyncOperation.h
@@ -1,12 +1,31 @@
 #import <JFFAsyncOperations/JFFAsyncOperationsBlockDefinitions.h>
-
 #import <Foundation/Foundation.h>
 
+
+/**
+A category to cancel operations associated to an object.
+Once the object is destroyed the operation gets cancelled.
+*/
 @interface NSObject (WeakAsyncOperation)
 
-//TODO20 immediately cancel callback
+
+/**
+Wraps an operation so that it gets cancelled once **self** object is deallocated.
+
+@param asyncOp An operation that performs some job asynchronously.
+
+@return An operation that will be cancelled once **self** gets deallocated.
+*/
 - (JFFAsyncOperation)autoUnsubsribeOnDeallocAsyncOperation:(JFFAsyncOperation)asyncOp;
 
+
+/**
+Wraps an operation so that its users stop receiving events from it once **self** object is deallocated.
+
+@param asyncOp An operation that performs some job asynchronously.
+
+@return Once **self** gets deallocated, the users will stop receiving events from the operation.
+*/
 - (JFFAsyncOperation)autoCancelOnDeallocAsyncOperation:(JFFAsyncOperation)asyncOp;
 
 @end
