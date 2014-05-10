a33bb975c0060633a253d09057568232997906f5
dodikk <dodikk88.reg@gmail.com>
2014-05-10 17:40:06 +0300

    [doc] Added reference for AsyncMap

--- lib/JFFAsyncOperations/Categories/NSArray+AsyncMap.h
+++ lib/JFFAsyncOperations/Categories/NSArray+AsyncMap.h
@@ -1,13 +1,53 @@
 #import <JFFAsyncOperations/JFFAsyncOperationsBlockDefinitions.h>
-
 #import <Foundation/Foundation.h>
 
+
+/**
+A category to perform map/reduce actions asynchronously in the background thread.
+*/
 @interface NSArray (AsyncMap)
 
+/**
+Creates an operation that performs the block with each array element simultaneously.
+The resulting array will be returned in the completion callback of the async operation.
+
+Note : if the block returns at least one error, the entire operation will be marked as "failed".
+The entire operation will also be cancelled.
+
+@param block A block to construct an asynchronous operation with the given array element.
+
+@return An asycnhronous operation that performs actions on array elements in parallel.
+*/
 - (JFFAsyncOperation)asyncMap:(JFFAsyncOperationBinder)block;
 
+
+
+/**
+Creates an operation that performs the block with each array element simultaneously.
+The resulting array will be returned in the completion callback of the async operation.
+
+Note : if the block returns at least one error, the entire operation will be marked as "failed".
+However, all operations will be attempted to execute.
+
+@param block A block to construct an asynchronous operation with the given array element.
+
+@return An asycnhronous operation that performs actions on array elements in parallel.
+*/
 - (JFFAsyncOperation)asyncWaitAllMap:(JFFAsyncOperationBinder)block;
 
+
+
+/**
+Creates an operation that performs the block with each array element simultaneously.
+The resulting array will be returned in the completion callback of the async operation.
+
+Note : if the block returns at least one error, the execution keeps going. 
+The operation is considered as failed if all operations have failed.
+
+@param block A block to construct an asynchronous operation with the given array element.
+
+@return An asycnhronous operation that performs actions on array elements in parallel.
+*/
 - (JFFAsyncOperation)tolerantFaultAsyncMap:(JFFAsyncOperationBinder)block;
 
 @end
--- lib/JFFAsyncOperations/Categories/NSDictionary+AsyncMap.h
+++ lib/JFFAsyncOperations/Categories/NSDictionary+AsyncMap.h
@@ -1,11 +1,26 @@
 #import <JFFAsyncOperations/JFFAsyncOperationsBlockDefinitions.h>
-
 #import <Foundation/Foundation.h>
+
 
 typedef JFFAsyncOperation (^JFFAsyncDictMappingBlock)(id key, id object);
 
+
+/**
+A category to perform map/reduce actions asynchronously in the background thread.
+*/
 @interface NSDictionary (AsyncMap)
 
+/**
+Creates an operation that performs the block with each dictionary key-value pair simultaneously.
+The resulting dictionary will be returned in the completion callback of the async operation.
+
+Note : if the block returns at least one error, the entire operation will be marked as "failed".
+The entire operation will also be cancelled.
+
+@param block A block to construct an asynchronous operation with the given key-value pair.
+
+@return An asycnhronous operation that performs actions on dictionary ke-value pair elements in parallel.
+*/
 - (JFFAsyncOperation)asyncMap:(JFFAsyncDictMappingBlock)block;
 
 @end
