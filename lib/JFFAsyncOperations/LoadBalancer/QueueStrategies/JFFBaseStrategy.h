54acc1adba121fe49dc383350ceef3668acc6f48
dodikk <dodikk88.reg@gmail.com>
2014-05-10 18:24:48 +0300

    [doc] Added some private API documentation]

--- lib/JFFAsyncOperations/LoadBalancer/QueueStrategies/JFFBaseStrategy.h
+++ lib/JFFAsyncOperations/LoadBalancer/QueueStrategies/JFFBaseStrategy.h
@@ -1,17 +1,34 @@
 #import <Foundation/Foundation.h>
 
-@class
-JFFQueueState,
-JFFBaseLoaderOwner;
+@class JFFQueueState;
+@class JFFBaseLoaderOwner;
 
+
+/**
+A base class for execution order strategy.
+*/
 @interface JFFBaseStrategy : NSObject
 {
 @protected
     JFFQueueState *_queueState;
 }
 
+/**
+A task launcher method.
+
+@param pendingLoader A worker block to execute.
+*/
 - (void)executePendingLoader:(JFFBaseLoaderOwner *)pendingLoader;
 
+
+/**
+A designated initializer.
+Note : other initializers are forbidden.
+
+@param queueState A storage for worker objects.
+
+@return A properly initialized strategy object.
+*/
 - (instancetype)initWithQueueState:(JFFQueueState *)queueState;
 
 @end
