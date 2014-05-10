a5cb87a6605b293816e9ad18f105b9b7f895eb6a
dodikk <dodikk88.reg@gmail.com>
2014-05-10 18:46:26 +0300

    [doc] Added reference for some functions

--- lib/JFFAsyncOperations/JFFAsyncOperationHelpers.h
+++ lib/JFFAsyncOperations/JFFAsyncOperationHelpers.h
@@ -14,12 +14,61 @@
 
 ///////////////////////// ADD OBSERVERS OF ASYNC OP. RESULT ////////////////////////
 
+
+/**
+Creates an asynchronouys operation that returns the specified object.
+
+@param result An object to return
+
+@return An asynchronous operation.
+*/
 OBJC_EXTERN    JFFAsyncOperation asyncOperationWithResult(id result);
+
+
+/**
+Creates an asynchronouys operation that always fails with the given error.
+
+@param error An error object to fail with
+
+@return An asynchronous operation.
+*/
 OBJC_EXTERN    JFFAsyncOperation asyncOperationWithError(NSError *error);
+
+
+/**
+Creates an asynchronouys operation that gets cancelled with the specified flag.
+The operation will be cancelled instead of being executed.
+
+@param canceled YES to cancel both the result operation and related ones.
+NO to unsubscribe the operation.
+
+@return An asynchronous operation.
+
+Note : such operations should be used in unit tests only.
+*/
 OBJC_EXTERN    JFFAsyncOperation asyncOperationWithCancelFlag(BOOL canceled);
+
+
+/**
+Creates an operation with an infinite loop within.
+
+@return An asynchronous operation.
+
+Note : such operations should be used in unit tests only.
+*/
 OBJC_EXTERN    JFFAsyncOperation neverFinishAsyncOperation(void);
+
     
+/**
+Creates an asynchronous operation with a synchronous one.
+
+@param loadDataBlock The worker block that returns data object or error (as an out parameter).
+Warning : if you pass nil an exception will be thrown.
+
+@return An asynchronous operation.
+*/
 OBJC_EXTERN    JFFAsyncOperation asyncOperationWithSyncOperationInCurrentQueue(JFFSyncOperation block);
+
     
     //finish_callback_block_ called before loader_'s JFFDidFinishAsyncOperationHandler
 OBJC_EXTERN    JFFAsyncOperation asyncOperationWithFinishCallbackBlock(JFFAsyncOperation loader,
