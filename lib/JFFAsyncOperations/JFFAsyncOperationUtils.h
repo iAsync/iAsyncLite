2a107089c2ea7c23a4f7d4bad18f93e4489301fe
dodikk <dodikk88.reg@gmail.com>
2014-05-10 18:39:37 +0300

    [doc] Added comments for async operation constructor functions

--- lib/JFFAsyncOperations/JFFAsyncOperationUtils.h
+++ lib/JFFAsyncOperations/JFFAsyncOperationUtils.h
@@ -5,16 +5,70 @@
 #include <objc/objc-api.h>
 
 
+/**
+Creates an asynchronous operation with a synchronous one.
+
+@param loadDataBlock The worker block that returns data object or error (as an out parameter).
+Warning : if you pass nil an exception will be thrown.
+
+@return An asynchronous operation.
+*/
 OBJC_EXTERN    JFFAsyncOperation asyncOperationWithSyncOperation(JFFSyncOperation loadDataBlock);
-    
+  
+
+
+/**
+Creates an asynchronous operation with a synchronous one and shedules it on an operation queue.
+
+@param loadDataBlock The worker block that returns data object or error (as an out parameter).
+Warning : if you pass nil an exception will be thrown.
+
+@param queueName The name of an operation queue. It will be taken from the pool or spawned by the dispatch_queue_create() call.
+
+@return An asynchronous operation.
+*/  
 OBJC_EXTERN    JFFAsyncOperation asyncOperationWithSyncOperationAndQueue(JFFSyncOperation loadDataBlock,
                                                               const char *queueName);
-    
+
+
+/**
+Creates an asynchronous operation with a synchronous one and shedules it on an operation queue.
+
+@param loadDataBlock The worker block that returns data object or error (as an out parameter).
+Warning : if you pass nil an exception will be thrown.
+
+@param queueName The name of an operation queue. It will be taken from the pool or spawned by the dispatch_queue_create() call.
+
+@param isSerialQueue YES for continuous execution. NO for parallel execution.
+
+@return An asynchronous operation.
+*/
 OBJC_EXTERN    JFFAsyncOperation asyncOperationWithSyncOperationAndConfigurableQueue(JFFSyncOperation loadDataBlock, const char *queueName, BOOL isSerialQueue);
     
+
+
+/**
+Creates an asynchronous operation with a synchronous one.
+It won't start executing as long as there are other operations in the queue.
+
+@param loadDataBlock The worker block that returns data object or error (as an out parameter).
+Warning : if you pass nil an exception will be thrown.
+
+@return An asynchronous operation.
+*/
 OBJC_EXTERN    JFFAsyncOperation barrierAsyncOperationWithSyncOperationAndQueue(JFFSyncOperation loadDataBlock,
                                                                      const char *queueName);
-    
+
+
+/**
+Creates an asynchronous operation with a synchronous one.
+A synchronous operation block accepts the progress callback.
+
+@param loadDataBlock The worker block that returns data object or error (as an out parameter).
+Warning : if you pass nil an exception will be thrown.
+
+@return An asynchronous operation.
+*/    
 OBJC_EXTERN    JFFAsyncOperation asyncOperationWithSyncOperationWithProgressBlock(JFFSyncOperationWithProgress progressLoadDataBlock);
 
 
