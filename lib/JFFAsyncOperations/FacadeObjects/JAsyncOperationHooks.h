1ac476d32c4a44c0177344b62ff5b2e6ada0e122
dodikk <dodikk88.reg@gmail.com>
2014-06-01 20:16:04 +0300

    [doc] Added done callback hooks

--- lib/JFFAsyncOperations/FacadeObjects/JAsyncOperationHooks.h
+++ lib/JFFAsyncOperations/FacadeObjects/JAsyncOperationHooks.h
@@ -15,11 +15,45 @@
 @interface JAsyncOperationHooks : NSObject
 
 /**
+ Subscribes the given block to operation completion.
+ The original callback will be executed.
+ 
+ @param loader An asynchronous operation to hook.
+ @param doneCallbackHook An action block to subscribe for completion.
+ 
+ @return A hooked asynchronous operation.
  */
 +(JFFAsyncOperation)asyncOperation:(JFFAsyncOperation)loader
-                     withDoneBlock:(JFFSimpleBlock)doneCallbackHook
+               withDoneActionBlock:(JFFSimpleBlock)doneCallbackHook
 __attribute__((nonnull, pure,const));
 
 
+
+/**
+ Subscribes the given block to operation completion.
+ The original callback will be executed.
+ 
+ @param loader An asynchronous operation to hook.
+ @param doneCallbackHook A block callback to subscribe for completion.
+ 
+ @return A hooked asynchronous operation.
+ */
++(JFFAsyncOperation)asyncOperation:(JFFAsyncOperation)loader
+             withDoneCallbackBlock:(JFFDidFinishAsyncOperationHandler)doneCallbackHook
+__attribute__((nonnull, pure,const));
+
+
+
+/**
+ Subscribes the given block to operation completion.
+ 
+ @param loader An asynchronous operation to hook.
+ @param doneCallbackHook A hook for completion. Depending on its implementation, the original callback will either be invoked or not.
+ 
+ @return A hooked asynchronous operation.
+ */
++(JFFAsyncOperation)asyncOperation:(JFFAsyncOperation)loader
+              withDoneCallbackHook:(JFFDidFinishAsyncOperationHook)doneCallbackHook
+__attribute__((nonnull, pure,const));
 
 @end
--- lib/JFFAsyncOperations/FacadeObjects/JAsyncOperationHooks.m
+++ lib/JFFAsyncOperations/FacadeObjects/JAsyncOperationHooks.m
@@ -1,17 +1,39 @@
 #import "JAsyncOperationHooks.h"
 
 #import "JFFAsyncOperationContinuity.h"
+#import "JFFAsyncOperationHelpers.h"
+
 
 @implementation JAsyncOperationHooks
 
 +(JFFAsyncOperation)asyncOperation:(JFFAsyncOperation)loader
-                     withDoneBlock:(JFFSimpleBlock)doneCallbackHook
+               withDoneActionBlock:(JFFSimpleBlock)doneCallbackHook
 {
     NSParameterAssert( nil!= loader           );
     NSParameterAssert( nil!= doneCallbackHook );
 
     return asyncOperationWithDoneBlock(loader, doneCallbackHook);
 }
+
+
++(JFFAsyncOperation)asyncOperation:(JFFAsyncOperation)loader
+             withDoneCallbackBlock:(JFFDidFinishAsyncOperationHandler)doneCallbackHook
+{
+    NSParameterAssert( nil!= loader           );
+    NSParameterAssert( nil!= doneCallbackHook );
+    
+    return asyncOperationWithFinishCallbackBlock(loader, doneCallbackHook);
+}
+
++(JFFAsyncOperation)asyncOperation:(JFFAsyncOperation)loader
+              withDoneCallbackHook:(JFFDidFinishAsyncOperationHook)doneCallbackHook
+{
+    NSParameterAssert( nil!= loader           );
+    NSParameterAssert( nil!= doneCallbackHook );
+    
+    return asyncOperationWithFinishHookBlock(loader, doneCallbackHook);
+}
+
 
 
 @end
