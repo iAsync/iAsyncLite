52ced04bcba3912034bd1fefc9401b43003c9b0e
dodikk <dodikk88.reg@gmail.com>
2014-06-01 21:13:25 +0300

    [doc] Added wrappers for finish callback hooks

--- lib/JFFAsyncOperations/FacadeObjects/JDoneCallbackAsyncOperationHooks.h
+++ lib/JFFAsyncOperations/FacadeObjects/JDoneCallbackAsyncOperationHooks.h
@@ -56,4 +56,57 @@ __attribute__((nonnull, pure,const));
               withDoneCallbackHook:(JFFDidFinishAsyncOperationHook)doneCallbackHook
 __attribute__((nonnull, pure,const));
 
+
+
+/**
+ Subscribes an action block to operation start and a callback block to operation completion.
+ A typical usage is injecting logging messages.
+ 
+ @param loader An asynchronous operation to hook.
+ @param startBlock An action block to subscribe for operation start.
+ @param finishCallback A block callback to subscribe for completion. The original callback will be executed.
+ 
+ @return A hooked asynchronous operation.
+ */
++(JFFAsyncOperation)asyncOperation:(JFFAsyncOperation)loader
+              withStartActionBlock:(JFFSimpleBlock)startBlock
+               finishCallbackBlock:(JFFDidFinishAsyncOperationHandler)finishCallback
+__attribute__((nonnull, pure,const));
+
+
+
+/**
+ Subscribes an action block to operation start and a callback block to operation completion.
+ Same as +[JDoneCallbackAsyncOperationHooks asyncOperation:withStartActionBlock:finishCallbackBlock:] but hook blocks may be nil
+ A typical usage is injecting logging messages.
+ 
+ @param loader An asynchronous operation to hook.
+ @param startBlock An action block to subscribe for operation start.
+ @param finishCallback A block callback to subscribe for completion. The original callback will be executed.
+ 
+ @return A hooked asynchronous operation.
+ */
++(JFFAsyncOperation)asyncOperation:(JFFAsyncOperation)loader
+      withOptionalStartActionBlock:(JFFSimpleBlock)startBlock
+       optionalFinishCallbackBlock:(JFFDidFinishAsyncOperationHandler)finishCallback
+__attribute__((nonnull(1), pure,const));
+
+
+/**
+ Subscribes an action block to operation start and a callback block to operation completion.
+ Same as +[JDoneCallbackAsyncOperationHooks asyncOperation:withStartActionBlock:finishCallbackBlock:] but hook blocks may be nil
+ 
+ A typical usage is injecting logging messages.
+ 
+ @param loader An asynchronous operation to hook.
+ @param startBlock An action block to subscribe for operation start.
+ @param doneBlock A block callback to subscribe for completion. The original callback will be executed.
+ 
+ @return A hooked asynchronous operation.
+ */
++(JFFAsyncOperation)asyncOperation:(JFFAsyncOperation)loader
+      withOptionalStartActionBlock:(JFFSimpleBlock)startBlock
+         optionalFinishActionBlock:(JFFSimpleBlock)doneBlock
+__attribute__((nonnull(1), pure,const));
+
 @end
--- lib/JFFAsyncOperations/FacadeObjects/JDoneCallbackAsyncOperationHooks.m
+++ lib/JFFAsyncOperations/FacadeObjects/JDoneCallbackAsyncOperationHooks.m
@@ -34,6 +34,33 @@
     return asyncOperationWithFinishHookBlock(loader, doneCallbackHook);
 }
 
++(JFFAsyncOperation)asyncOperation:(JFFAsyncOperation)loader
+              withStartActionBlock:(JFFSimpleBlock)startBlock
+               finishCallbackBlock:(JFFDidFinishAsyncOperationHandler)finishCallback
+{
+    NSParameterAssert( nil!= loader         );
+    NSParameterAssert( nil!= startBlock     );
+    NSParameterAssert( nil!= finishCallback );
+    
+    return asyncOperationWithStartAndFinishBlocks(loader, startBlock, finishCallback);
+}
 
++(JFFAsyncOperation)asyncOperation:(JFFAsyncOperation)loader
+      withOptionalStartActionBlock:(JFFSimpleBlock)startBlock
+       optionalFinishCallbackBlock:(JFFDidFinishAsyncOperationHandler)finishCallback
+{
+    NSParameterAssert( nil!= loader );
+    
+    return asyncOperationWithOptionalStartAndFinishBlocks( loader, startBlock, finishCallback );
+}
+
++(JFFAsyncOperation)asyncOperation:(JFFAsyncOperation)loader
+      withOptionalStartActionBlock:(JFFSimpleBlock)startBlock
+         optionalFinishActionBlock:(JFFSimpleBlock)doneBlock
+{
+    NSParameterAssert( nil!= loader );
+    
+    return asyncOperationWithStartAndDoneBlocks( loader, startBlock, doneBlock );
+}
 
 @end
